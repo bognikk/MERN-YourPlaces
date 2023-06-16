db912a2b822dbaecf79d37e684c554c2c984fca9
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-06-16 10:46:33 -0400

    UserList comp

---
+++ front/src/user/components/UserItem.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./UserItem.css";
+
+const UserItem = () => {
+	return (
+		<>
+			<h2>UserItem Works!</h2>;
+		</>
+	);
+};
+
+export default UserItem;
--- front/src/user/pages/Users.jsx
+++ front/src/user/pages/Users.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-	return (
-		<>
-			<h2>Users Works!</h2>;
-		</>
-	);
+	const USERS = [
+		{
+			id: "u1",
+			name: "Nikola Bogicevic",
+			image: "https://avatars.githubusercontent.com/u/29068353?v=4",
+			places: 3,
+		},
+		{
+			id: "u2",
+			name: "Lena Adalena",
+			image:
+				"https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
+			places: 5,
+		},
+	];
+
+	return <UsersList items={USERS} />;
 };
 
 export default Users;
