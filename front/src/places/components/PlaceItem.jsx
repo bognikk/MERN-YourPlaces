7735cbbabcf7a62974c6b6c3c928894c39990fff
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-06-24 16:59:53 -0400

    rendering a map with google

--- front/src/places/components/PlaceItem.jsx
+++ front/src/places/components/PlaceItem.jsx
@@ -3,6 +3,7 @@ import React, { useContext, useState } from "react";
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
 import Modal from "../../shared/components/UIElements/Modal";
+import Map from "../../shared/components/UIElements/Map";
 import { AuthContext } from "../../shared/context/auth-context";
 import "./PlaceItem.scss";
 
@@ -39,7 +40,7 @@ const PlaceItem = (props) => {
 				footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
 			>
 				<div className="map-container">
-					<h2>THE MAP!</h2>
+					<Map center={props.coordinates} zoom={16} />
 				</div>
 			</Modal>
 			<Modal
