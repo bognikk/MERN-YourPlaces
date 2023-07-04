a465a0d8a8d5a63a1d5e2213721c6acb2a732a84
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-07-04 15:27:06 -0400

    deleting places and removing it from the user

--- back/controllers/places-controllers.js
+++ back/controllers/places-controllers.js
@@ -198,7 +198,7 @@ const deletePlace = async (req, res, next) => {
 
 	let place;
 	try {
-		place = await Place.findById(placeId);
+		place = await Place.findById(placeId).populate("creator");
 	} catch (err) {
 		const error = new HttpError(
 			"Something went wrong, could not delete a place.",
@@ -207,8 +207,18 @@ const deletePlace = async (req, res, next) => {
 		return next(error);
 	}
 
+	if (!place) {
+		const error = new HttpError("Could not find place for this id.", 404);
+		return next(error);
+	}
+
 	try {
-		await place.remove();
+		const sess = await mongoose.startSession();
+		sess.startTransaction();
+		await place.deleteOne({ session: sess });
+		place.creator.places.pull(place);
+		await place.creator.save({ session: sess });
+		await sess.commitTransaction();
 	} catch (err) {
 		const error = new HttpError(
 			"Deleting place failed, something went wrong.",
@@ -217,7 +227,7 @@ const deletePlace = async (req, res, next) => {
 		return next(error);
 	}
 
-	res.status(200).json({ message: "Deleted place." });
+	res.status(200).json({ message: "Place deleted." });
 };
 
 exports.getPlaceById = getPlaceById;
