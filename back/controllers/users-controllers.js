797ee8f845596c1943a4ebf6d39551dbf073d9bb
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-07-04 13:40:34 -0400

    adding the relation between places and users

--- back/controllers/users-controllers.js
+++ back/controllers/users-controllers.js
@@ -41,7 +41,7 @@ const signup = async (req, res, next) => {
 		);
 	}
 
-	const { name, email, password, places } = req.body;
+	const { name, email, password } = req.body;
 
 	let existingUser;
 	try {
@@ -68,7 +68,7 @@ const signup = async (req, res, next) => {
 		image:
 			"https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
 		password,
-		places,
+		places: [],
 	});
 
 	try {
