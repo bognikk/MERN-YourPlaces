1abb51cc14ebb3049f6e476a3bb8802199c8f547
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-07-05 10:38:03 -0400

    sending a POST request to the backend

--- back/app.js
+++ back/app.js
@@ -12,6 +12,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
+	);
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+	next();
+});
+
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
@@ -31,7 +42,7 @@ app.use((error, req, res, next) => {
 
 mongoose
 	.connect(
-		`mongodb+srv://nikola:${DATABASE_PASSWORD}@cluster0.soncsra.mongodb.net/places?retryWrites=true&w=majority`
+		`mongodb+srv://nikola:${DATABASE_PASSWORD}@cluster0.soncsra.mongodb.net/mern?retryWrites=true&w=majority`
 	)
 	.then(() => {
 		app.listen(5000);
--- back/controllers/users-controllers.js
+++ back/controllers/users-controllers.js
@@ -1,17 +1,7 @@
-const uuid = require("uuid").v4;
 const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
-
-const DUMMY_USERS = [
-	{
-		id: "u1",
-		name: "Nikola Bogicevic",
-		email: "nbogicevic2@gmail.com",
-		password: "testTest",
-	},
-];
 
 // -------------------------------- GET --------------------------------
 const getUsers = async (req, res, next) => {
