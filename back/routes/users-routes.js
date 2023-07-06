bd8ddabecd24968e84d662703f80cceb4474fa0f
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-07-06 17:59:09 -0400

    multer for saving files

--- back/routes/users-routes.js
+++ back/routes/users-routes.js
@@ -2,11 +2,14 @@ const express = require("express");
 const { check } = require("express-validator");
 
 const usersControllers = require("../controllers/users-controllers");
+const fileUpload = require("../middleware/file-upload");
+
 const router = express.Router();
 
 router.get("/", usersControllers.getUsers);
 router.post(
 	"/signup",
+	fileUpload.single("image"),
 	[
 		check("email").not().isEmpty(),
 		check("email").normalizeEmail().isEmail(), // Test@test.com => test@test.com
