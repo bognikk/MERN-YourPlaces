00d8c1b1cdbbc0f4b9bf8e8631c46d73bbd910ba
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-07-07 10:31:20 -0400

    connecting users to images

--- back/controllers/users-controllers.js
+++ back/controllers/users-controllers.js
@@ -65,8 +65,7 @@ const signup = async (req, res, next) => {
 	const createdUser = new User({
 		name,
 		email,
-		image:
-			"https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
+		image: req.file.path,
 		password,
 		places: [],
 	});
