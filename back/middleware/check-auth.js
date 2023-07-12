63e0390395a01015e6195d5c39607fd8c96adce5
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-07-11 22:44:52 -0400

    using jwt in react

--- back/middleware/check-auth.js
+++ back/middleware/check-auth.js
@@ -5,6 +5,9 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const HttpError = require("../models/http-error");
 
 module.exports = (req, res, next) => {
+	if (req.method === "OPTIONS") {
+		return next();
+	}
 	try {
 		const token = req.headers.authorization.split(" ")[1]; // Authorization: 'Bearer TOKEN'
 		if (!token) {
