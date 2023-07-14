b99259f979611786eb60a710a079b340370a15c6
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-07-14 10:19:14 -0400

    react lazy load pages

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 
 import {
 	BrowserRouter as Router,
@@ -8,13 +8,16 @@ import {
 } from "react-router-dom";
 
 import Users from "./user/pages/Users";
-import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
-import UserPlaces from "./places/pages/UserPlaces";
-import UpdatePlace from "./places/pages/UpdatePlace";
-import Auth from "./user/pages/Auth";
+import LoadingSpinner from "./shared/components/UIElements/LoadingSpinner";
+
 import { AuthContext } from "./shared/context/auth-context";
 import { useAuth } from "./shared/hooks/auth-hook";
+
+const NewPlace = React.lazy(() => import("./places/pages/NewPlace"));
+const UserPlaces = React.lazy(() => import("./places/pages/UserPlaces"));
+const UpdatePlace = React.lazy(() => import("./places/pages/UpdatePlace"));
+const Auth = React.lazy(() => import("./user/pages/Auth"));
 
 const App = () => {
 	const { token, login, logout, userId } = useAuth();
@@ -68,7 +71,17 @@ const App = () => {
 		>
 			<Router>
 				<MainNavigation />
-				<main>{routes}</main>
+				<main>
+					<Suspense
+						fallback={
+							<div className="center">
+								<LoadingSpinner />
+							</div>
+						}
+					>
+						{routes}
+					</Suspense>
+				</main>
 			</Router>
 		</AuthContext.Provider>
 	);
